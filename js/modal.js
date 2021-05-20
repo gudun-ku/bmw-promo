f30cdd7129c7a788d85333666b9514e183078ede
Белоушкин Александр Николаевич (4042053) <anbeloushkin@dev.vtb>
2021-05-20 18:43:57 +0300

    adds day3 code

---
+++ js/modal.js
@@ -0,0 +1,22 @@
+const moreElems = document.querySelectorAll('.more');
+const modalElem = document.querySelector('.modal');
+
+const openModal = () => {
+    modalElem.classList.remove('hidden');
+};
+
+const closeModal = () => {
+    modalElem.classList.add('hidden');
+};
+
+moreElems.forEach((moreElem) => {
+    moreElem.addEventListener('click', openModal);
+})
+
+modalElem.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.classList.contains('overlay') ||
+        target.classList.contains('modal__close')) {
+            closeModal();
+    }
+});
