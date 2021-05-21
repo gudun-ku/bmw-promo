af9de9070af5c7362aa78135a47ec1dd3b5bb513
Белоушкин Александр Николаевич (4042053) <anbeloushkin@dev.vtb>
2021-05-21 17:48:09 +0300

    changes accordion.js following translation

--- js/accordion.js
+++ js/accordion.js
@@ -2,24 +2,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const featureLinkElems = document.querySelectorAll('.feature__link');
     const featureSubElems = document.querySelectorAll('.feature-sub');
 
-    featureLinkElems.forEach((btn, index) => {
+    const handlerAccordion = (btn, index) => {
+
         btn.addEventListener('click', () => {
-            let isActive = false;
-            // save current active state before updating class lists
-            if (btn.classList.contains('feature__link_active')) {
-                isActive = true;
-            }
-            featureSubElems.forEach((lnkelem) => {
-                lnkelem.classList.add('hidden');
+            featureSubElems.forEach((featureSubElem, i) => {
+                if (index === i) {
+                    featureSubElems[index].classList.toggle('hidden');
+                } else {
+                    featureSubElem.classList.add('hidden');
+                }
             });
-            featureLinkElems.forEach((btnelem) => {
-                btnelem.classList.remove('feature__link_active')
+    
+            featureLinkElems.forEach(featureLinkElem => {
+                if (featureLinkElem === btn) {
+                    btn.classList.toggle('feature__link_active');
+                } else {
+                    featureLinkElem.classList.remove('feature__link_active');
+                }
             });
-            if (!isActive) {
-                featureSubElems[index].classList.remove('hidden');          
-                btn.classList.add('feature__link_active');
-            }
-        })
-    })
+        });        
+    }
+
+    featureLinkElems.forEach(handlerAccordion);
 
 });
